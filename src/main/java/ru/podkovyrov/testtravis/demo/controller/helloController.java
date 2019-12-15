730f08f8ef538ed3df3d045f0992a3e65703e047
Denis <denis.podkovyrov@gmail.com>
2019-12-16 00:03:47 +0300

    added helloWithUsernameController

--- src/main/java/ru/podkovyrov/testtravis/demo/controller/helloController.java
+++ src/main/java/ru/podkovyrov/testtravis/demo/controller/helloController.java
@@ -1,6 +1,8 @@
 package ru.podkovyrov.testtravis.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +10,10 @@ public class helloController {
     @GetMapping("/hello")
     public String welcome(){
         return "Hello, world!";
+    }
+
+    @GetMapping(path = "/hello", params = "name")
+    public String welcomeUsername(@RequestParam("name") String name){
+        return "Hello, " + name + "!";
     }
 }
