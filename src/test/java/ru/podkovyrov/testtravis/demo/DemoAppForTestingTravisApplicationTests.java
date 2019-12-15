110ff3e21ab001272aef297a70d9e43ff6c62ef6
Denis <denis.podkovyrov@gmail.com>
2019-12-16 00:00:27 +0300

    added test for helloWithUsernameController

---
+++ src/test/java/ru/podkovyrov/testtravis/demo/DemoAppForTestingTravisApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.podkovyrov.testtravis.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAppForTestingTravisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- src/test/java/ru/podkovyrov/testtravis/demo/controllerTest.java
+++ src/test/java/ru/podkovyrov/testtravis/demo/controllerTest.java
@@ -1,4 +1,41 @@
 package ru.podkovyrov.testtravis.demo;
 
-public class controllerTest {
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class  controllerTest {
+
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    void testHello() throws Exception{
+
+        mvc.perform(
+                get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello, world!"));
+
+    }
+
+    @Test
+    void testHelloWithUsername() throws Exception{
+
+        mvc.perform(
+                get("/hello")
+                        .param("name", "Neo"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello, Neo!"));
+
+    }
+
 }
